c44bac2e9c4c27b6665e8107d9cecc5a89f2180b
nyanchan <personal@nyanchangames.com>
2020-03-08 21:13:47 +0900

    Add server code

---
+++ server/src/index.js
@@ -0,0 +1,15 @@
+{
+  "name": "karuta server",
+  "version": "1.0.0",
+  "description": "karuta server",
+  "main": "src/index.js",
+  "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1",
+    "start": "node src/index.js"
+  },
+  "author": "Yukio Nozawa",
+  "license": "ISC",
+  "dependencies": {
+    "websocket": "^1.0.31"
+  }
+}
